f06da16184d27658ac760172df74f957b440d737
Esterling Accime <esterling.accime@coxautoinc.com>
2019-02-18 02:20:59 -0500

    adding message

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const routes = require('./routes');
 // Define Global Variables
 const app = express();
 const log = console.log;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080; // Step 1
 
 
-mongoose.connect('mongodb://localhost/my_database', {
+// Step 2
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
     useNewUrlParser: true
 });
 
@@ -23,6 +24,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
 
 app.listen(PORT, () => {
     log(`Server is starting at PORT: ${PORT}`);
